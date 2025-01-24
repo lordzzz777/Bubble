e61c93bea23fff9c0ab26329e3645e7d796aaf48
lordzzz <lordzzz8@gmail.com>
2025-01-24 22:49:28 +0100

    Initial Commit

---
+++ Bubble/BubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleApp.swift
+//  Bubble
+//
+//  Created by Esteban Pérez Castillejo on 24/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bubble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bubble
+//
+//  Created by Esteban Pérez Castillejo on 24/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
