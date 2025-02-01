85994a81a6d4b39f9f930741fc4629cf4b8a7b88
lordzzz <lordzzz8@gmail.com>
2025-02-01 04:43:22 +0100

    Crear Maquete de listado de chats

--- Bubble/Preview Content/DataListMock.swift
+++ Bubble/Preview Content/DataListMock.swift
@@ -15,12 +15,12 @@ struct ModelListMock: Identifiable {
 }
 
 @MainActor let dataList: [ModelListMock] = [
-    .init(nameAlias: "TopGum", nameImage: "topgum", dataTimer: "01:50"),
+  .init(nameAlias: "TopGum", nameImage: "topgum", dataTimer: "01:50"),
     .init(nameAlias: "Peterete", nameImage: "peterete", dataTimer: "18:20"),
     .init(nameAlias: "Paloma23", nameImage: "paloma", dataTimer: "16:30"),
     .init(nameAlias: "Yeikobu", nameImage: "yey", dataTimer: "09:30"),
-    .init(nameAlias: "Lordzzz", nameImage: "lordzzz", dataTimer: "10:15"),
+    .init(nameAlias: "Lordzzz", nameImage: "lordzzz", dataTimer: "10:15")/*,
     .init(nameAlias: "Lovezno", nameImage: "lovezno", dataTimer: "11:00"),
     .init(nameAlias: "Monica36", nameImage: "monica", dataTimer: "20:07"),
-    .init(nameAlias: "Veronica27", nameImage: "veronica", dataTimer: "21:07")
+    .init(nameAlias: "Veronica27", nameImage: "veronica", dataTimer: "21:07")*/
 ]
--- Bubble/View/HomeView.swift
+++ Bubble/View/HomeView.swift
@@ -11,10 +11,10 @@ struct HomeView: View {
     // Esta es la variable que almacenará el valor seleccionado del Picker
     @State private var selectedVisibility = "privado"
     @State private var searchText = ""
-    
+    @State private var isWiffi = false
     // Esta es la lista de opciones para el Picker
     private let visibilityOptions = ["privado", "Publico"]
- 
+    
     var filteredData: [ModelListMock] {
         if searchText.isEmpty {
             return dataList
@@ -26,57 +26,68 @@ struct HomeView: View {
     var body: some View {
         NavigationStack{
             // Usamos un Picker con un estilo segmentado
-            Picker("Visibilidad", selection: $selectedVisibility) {
-                ForEach(visibilityOptions, id: \.self) { option in
-                    Text(option).tag(option) // Asegúrate de usar .tag para asociar cada Text con su valor
+            VStack{
+                Picker("Visibilidad", selection: $selectedVisibility) {
+                    ForEach(visibilityOptions, id: \.self) { option in
+                        Text(option).tag(option) // Asegúrate de usar .tag para asociar cada Text con su valor
+                    }
                 }
-            }
-            .pickerStyle(.segmented)
+                .pickerStyle(.segmented)
                 .padding()
-                List{
+                
+                if dataList.isEmpty{
+                    Text("No tienes chats aún").font(.largeTitle.bold())
+                        .padding(.bottom, 20)
+                    Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
+                        .font(.system(size: 100))
+                    Spacer()
                     
-                    ForEach(dataList){ item in
-                        NavigationLink(destination: {
-                            
-                        }, label: {
-                            HStack{
-                                Circle().frame(width: 50, height: 50, alignment: .center)
-                                    .overlay(content: {
-                                        Image(item.nameImage).resizable()
-                                            .scaledToFill()
+                }else{
+                    List{
+                        ForEach(dataList){ item in
+                            NavigationLink(destination: {
+                                
+                            }, label: {
+                                HStack{
+                                    Circle().frame(width: 50, height: 50, alignment: .center)
+                                        .overlay(content: {
+                                            Image(item.nameImage).resizable()
+                                                .scaledToFill()
                                             
-                                    }).clipShape(Circle())
-                                Text(item.nameAlias)
-                                Spacer()
-                                Text(item.dataTimer)
-                            } .swipeActions(content: {
-                                Button("borrar", systemImage: "trash.fill", role: .destructive, action: {
-                                    // ... Lógica eliminar
+                                        }).clipShape(Circle())
+                                    Text(item.nameAlias)
+                                    Spacer()
+                                    Text(item.dataTimer)
+                                } .swipeActions(content: {
+                                    Button("borrar", systemImage: "trash.fill", role: .destructive, action: {
+                                        // ... Lógica eliminar
+                                    })
                                 })
+                                
                             })
-                            
-                        })
-
- 
+                        }
                         
                     }
                     
                 }
-                .navigationTitle("Chat")
-                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always)) {
-                    ForEach(visibilityOptions, id: \.self) { option in
-                        Text(option).searchCompletion(option)
-                    }
+            }
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always)) {
+                ForEach(visibilityOptions, id: \.self) { option in
+                    Text(option).searchCompletion(option)
                 }
-               
-                .toolbar(content: {
-                    Button(action: {
-                        // ... Logica
-                    }, label: {
-                        Image(systemName: "plus")
-                    })
+            }
+            .navigationTitle("Chat")
+            .toolbar(content: {
+                Button(action: {
+                    // ... Logica
+                }, label: {
+                    Image(systemName: "plus")
                 })
+            })
         }
+        
+        
+        
     }
 }
 
