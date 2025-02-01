67a8feaff575f15339fb4d687e2125458002ad02
lordzzz <lordzzz8@gmail.com>
2025-02-01 23:35:41 +0100

    add sevices file FirebaseService

--- Bubble/Services/FirebaseService.swift
+++ Bubble/Services/FirebaseService.swift
@@ -2,7 +2,38 @@
 //  FirebaseService.swift
 //  Bubble
 //
+//  Created by Jacob Aguilar on 1/31/25.
+//
+//
 //  Created by Esteban Pérez Castillejo on 1/2/25.
 //
 
+
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+actor FirebaseService {
+
+    
+    // Traerme los chas de este usuario en tiempo real
+    // Leeer documentacion de Fire Stora
+    func fetchMessages(for chatID: String, completion: @escaping([MessagesModels]) -> Void){
+        
+        let db = Firestore.firestore()
+        let messageRef = db.collection("chats").document(chatID).collection("messages")
+        
+        messageRef.order(by: "timestamp", descending: false).getDocuments{ snapshot, error in
+            guard let document = snapshot?.documents, error == nil else {
+                print("error al optener Mensaje: \(error?.localizedDescription ?? "Desconocido")")
+                completion([])
+                return
+            }
+            let messages = document.compactMap { doc -> MessagesModels? in
+                try? doc.data(as: MessagesModels.self)
+            }
+            
+            completion(messages)
+        }
+    }
+}
