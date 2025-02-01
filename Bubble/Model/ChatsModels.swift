3fc79df185b1c912c6a6210512e4dadb5b2e8a6b
lordzzz <lordzzz8@gmail.com>
2025-02-01 23:40:33 +0100

    add model data file ChatsModels and crete strucf

---
+++ Bubble/Model/ChatsModels.swift
@@ -0,0 +1,28 @@
+//
+//  ChatsModels.swift
+//  Bubble
+//
+//  Created by Esteban Pérez Castillejo on 1/2/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct ChatsModels: Codable, Hashable {
+    var participants: [String] = []
+    var lastMessage: String
+    var lastMessageTimestamp: Timestamp
+}
+
+struct MessagesModels: Codable, Hashable {
+    var senderID: String
+    var countent: String
+    var timestamp: Timestamp
+    var type: MessageType
+}
+
+enum MessageType: String, Codable {
+    case text
+    case image
+    case video
+}
