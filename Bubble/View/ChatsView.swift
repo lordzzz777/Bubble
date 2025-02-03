2468504692a2cf61afc106006db5c24f19c61223
Esteban <114311106+lordzzz777@users.noreply.github.com>
2025-02-03 22:34:07 +0100

    Feature/3 vista chats vistas de listado de chats (#11)
    * Pintada la vista de lista de chas
    
    * Crear maketa para mostrar listado de chats
    
    * Crear Maquete de listado de chats
    
    * add file sevice  in the branch crear-nuevo-cuenta
    
    * add sevices file FirebaseService
    
    * add model data file ChatsModels and crete strucf
    
    * Efectuada tarea propuesta para revision del criterio de Aceptación Integrar carga de chats desde Firebase.
    
    * Solucionado un problema que impedia que la la lista se visualizara
    
    * Se maneja correctamente el caso de falta de conexión con un mensaje informativo.
    
    * creada la alerta de el error de carga mostrando un mensaje y permitiendo reintentar.
    
    * actualizado latarea Vistas de Listado de Chats "Vistas de Listado de Chats"
    
    * solucionado errores de diseño, y actualización de nombre

---
+++ Bubble/View/ChatsView.swift
@@ -0,0 +1,106 @@
+//
+//  HomeView.swift
+//  Bubble
+//
+//  Created by Esteban Pérez Castillejo on 30/1/25.
+//
+
+import SwiftUI
+import FirebaseFirestore
+import FirebaseCore
+
+struct ChatsView: View {
+    @State var viewModel = ChatViewModel()
+    
+    // Esta es la variable que almacenará el valor seleccionado del Picker
+    @State private var selectedVisibility = "privado"
+    @State private var searchText = ""
+    @State private var isWiffi = false
+    
+    // Esta es la lista de opciones para el Picker
+    private let visibilityOptions = ["privado", "Publico"]
+    
+    var filteredData: [ModelListMock] {
+        if searchText.isEmpty {
+            return dataList
+        } else {
+            return dataList.filter { $0.nameAlias.lowercased().contains(searchText.lowercased()) }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack{
+            // Usamos un Picker con un estilo segmentado
+            VStack{
+                Picker("Visibilidad", selection: $selectedVisibility) {
+                    ForEach(visibilityOptions, id: \.self) { option in
+                        Text(option).tag(option) // Asegúrate de usar .tag para asociar cada Text con su valor
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding()
+                
+                if $viewModel.chats.isEmpty{
+                    Text("No tienes chats aún").font(.largeTitle.bold())
+                        .padding(.bottom, 20)
+                    Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
+                        .font(.system(size: 100))
+                    Spacer()
+                    
+                }else{
+                    
+                    List{
+                        ForEach(viewModel.chats, id:\.lastMessageTimestamp){ chat in
+                            NavigationLink(destination: {
+                                
+                            }, label: {
+                                VStack(alignment: .leading){
+                                    let id1 = chat.participants[1]
+                                    let timestamp: Timestamp =  chat.lastMessageTimestamp
+                                    let lastMessage = chat.lastMessage
+                                    ListChatRowView(userID: id1, lastMessage: lastMessage, timestamp: timestamp)
+                                }
+                                .swipeActions(content: {
+                                    Button("borrar", systemImage: "trash.fill", role: .destructive, action: {
+                                        // ... Lógica eliminar
+                                    })
+                                })
+                                
+                            })
+                        }
+                        
+                    }
+                    
+                }
+            }
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always)) {
+                ForEach(visibilityOptions, id: \.self) { option in
+                    Text(option).searchCompletion(option)
+                }
+            }
+            .navigationTitle("Chats")
+            .onAppear {
+                viewModel.fetchChats()
+            }
+            .alert(isPresented: $viewModel.isfetchChatsError) {
+                Alert(title: 
+                        Text("Error al cargar los chats"),
+                      message: Text("Puede intentar nuevamente."),
+                      dismissButton: .default(Text("OK"))
+                )
+            }
+            .toolbar(content: {
+                Button(action: {
+                    // ... Logica
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            })
+        }
+    }
+    
+}
+
+#Preview {
+    ChatsView()
+}
