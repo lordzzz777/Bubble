ccbf065bd6602dc7b61a403f35b0560c42b3d93b
Yeikobu <aguilar.campos.jacob@gmail.com>
2025-02-09 01:35:34 -0300

    Agrega funcionalidad para obtener coincidencias de firebase a través de nickname

--- Bubble/Services/AddNewFriendService.swift
+++ Bubble/Services/AddNewFriendService.swift
@@ -17,7 +17,7 @@ actor AddNewFriendService {
         do {
             let documents = try await database.collection("users")
                 .whereField("nickname", isGreaterThanOrEqualTo: nickname)
-                .whereField("nickname", isLessThanOrEqualTo: nickname + "\u{f8ff}") // Carácter Unicode especial para el final del rango
+                .whereField("nickname", isLessThanOrEqualTo: nickname + "\u{f8ff}") // Carácter Unicode especial para el final del rango // Esto me ayudó a entender que se podía https://qiita.com/TKG_KM/items/2678664d975812bea6c8
                 .getDocuments()
             let documentsData = documents.documents.compactMap({$0})
             let matchedFriends = try documentsData.map { try $0.data(as: UserModel.self )}
