fbd6141bfe26f4046f5c36cee3e3fb4477868300
Yeikobu <aguilar.campos.jacob@gmail.com>
2025-02-08 23:41:55 -0300

    Reestructura código de ChatsView

--- Bubble/View/ChatsView.swift
+++ Bubble/View/ChatsView.swift
@@ -11,50 +11,50 @@ import FirebaseCore
 
 struct ChatsView: View {
     @Bindable var viewModel = ChatViewModel()
-    
-    // Esta es la variable que almacenará el valor seleccionado del Picker
 
     @State private var chatIdSelected: String = ""
     
     var body: some View {
-        NavigationStack{
-            
+        NavigationStack {
             // Usamos un Picker con un estilo segmentado
             VStack{
                 Picker("Visibilidad", selection: $viewModel.selectedVisibility) {
                     ForEach(viewModel.visibilityOptions, id: \.self) { option in
-
-                        Text(option).tag(option) // Asegúrate de usar .tag para asociar cada Text con su valor
+                        Text(option)
+                            .tag(option) // Asegúrate de usar .tag para asociar cada Text con su valor
                     }
                 }
                 .pickerStyle(.segmented)
                 .padding()
                 
-                if $viewModel.chats.isEmpty{
-                    Text("No tienes chats aún").font(.largeTitle.bold())
+                if $viewModel.chats.isEmpty {
+                    Text("No tienes chats aún")
+                        .font(.largeTitle.bold())
                         .padding(.bottom, 20)
+                    
                     Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
                         .font(.system(size: 100))
-                    Spacer()
                     
-                }else{
+                    Spacer()
+                } else {
                     
                     List{
-                        ForEach(viewModel.chats, id:\.lastMessageTimestamp){ chat in
+                        ForEach(viewModel.chats, id:\.lastMessageTimestamp) { chat in
                             
                             NavigationLink(destination: {
                                 
                             }, label: {
-                                VStack(alignment: .leading){
+                                VStack(alignment: .leading) {
                                     ListChatRowView(userID:viewModel.getFriendID(chat.participants), lastMessage: chat.lastMessage, timestamp: chat.lastMessageTimestamp)
 
                                 }
                             })
                             .swipeActions(content: {
                                 Button("borrar", systemImage: "trash.fill", action: {
-                                    if let chatID = chat.id{
+                                    if let chatID = chat.id {
                                         chatIdSelected = chatID
                                     }
+                                    
                                     viewModel.isMessageDestructive = true
                                     
                                 })
@@ -68,62 +68,43 @@ struct ChatsView: View {
                     
                 }
             }
-
             .searchable(text: $viewModel.searchQuery, placement: .navigationBarDrawer(displayMode: .always)) {
                 ForEach(viewModel.visibilityOptions, id: \.self) { option in
-
                     Text(option).searchCompletion(option)
                 }
             }
             .navigationTitle("Chats")
-
             .task {
-
                 await viewModel.fetchCats()
             }
-            
             // Alerta de Error
             .alert(isPresented: $viewModel.isfetchChatsError) {
-
-                Alert(title:
-
-                        Text(viewModel.errorTitle),
-                      message: Text(viewModel.errorDescription),
-                      dismissButton: .default(Text("OK"))
+                Alert(title: Text(viewModel.errorTitle), message: Text(viewModel.errorDescription), dismissButton: .default(Text("OK"))
                 )
             }
-            
             // Alerta de corfimación
             .alert(isPresented: $viewModel.isSuccessMessas) {
-                Alert(title:
-                        Text(viewModel.successMessasTitle),
-                      message: Text(viewModel.successMessasDescription),
-                      dismissButton: .default(Text("OK"))
-                )
+                Alert(title: Text(viewModel.successMessasTitle), message: Text(viewModel.successMessasDescription), dismissButton: .default(Text("OK")))
             }
-
-            
             // Alerta de advertencia antes de eliminar el chat
             .alert("⚠️ Eliminar Chat", isPresented: $viewModel.isMessageDestructive, actions: {
                 Button("Eliminar") {
-                        viewModel.deleteChat(chatID: chatIdSelected)
+                    viewModel.deleteChat(chatID: chatIdSelected)
                 }
                 
-                Button("Cancelar", role: .cancel) {
-                    // ... Acción para cancelar
-                }
-             },
-                   message: {
+                Button("Cancelar", role: .cancel) {}
+            }, message: {
                 Text("Si confirmas, se eliminará el Chat y la conversación de forma permanente y no podrás recuperarla. ¿Deseas continuar?")
             })
-            
-            .toolbar(content: {
-                Button(action: {
-                    // ... Logica
-                }, label: {
-                    Image(systemName: "plus")
-                })
-            })
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
+            }
         }
     }
 
