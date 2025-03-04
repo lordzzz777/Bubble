283ae56281dd419438e2c20701d3e867090f9e25
Yeikobu <aguilar.campos.jacob@gmail.com>
2025-03-04 19:30:19 -0300

    Crea textfield para enviar mensaje

--- Bubble/View/ChatsViews/PrivateChatView.swift
+++ Bubble/View/ChatsViews/PrivateChatView.swift
@@ -11,6 +11,7 @@ struct PrivateChatView: View {
     
     @Environment(ChatsViewModel.self) private var chatsViewModel
     @State private var privateChatViewModel = PrivateChatViewModel()
+    @State private var messageText: String = ""
     var user: UserModel
     var chat: ChatModel
     
@@ -38,15 +39,41 @@ struct PrivateChatView: View {
                         }
                     }
                 }
-                .toolbar {
-                    ToolbarItem(placement: .primaryAction) {
+                
+                Spacer()
+                
+                ZStack(alignment: .bottomTrailing) {
+                    TextField("Escribe tu mensaje", text: $messageText)
+                        .padding(.trailing, 20)
+                    
+                    if !messageText.isEmpty {
                         Button {
-                            
+                            messageText = ""
                         } label: {
-                            Image(systemName: "ellipsis.circle")
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundStyle(.gray)
                         }
-                        
                     }
+                }
+                .padding(8)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 8)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.gray, lineWidth: 0.3)
+                )
+                .padding(.bottom, 8)
+                .padding(.horizontal, 4)
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "ellipsis.circle")
+                    }
+                    
                 }
             }
             .navigationTitle(user.nickname)
