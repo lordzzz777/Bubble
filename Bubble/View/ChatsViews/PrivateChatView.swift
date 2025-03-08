104481106cf227f2bb71420254881a52d0ad7765
Jacob Aguilar <69016178+yeikobu@users.noreply.github.com>
2025-03-08 19:46:13 -0300

    Feature/t 54 agregar funcionalidad de mostrar hora en cada mensaje y el chat (#63)
    * Reemplaza funcionaliadad de formateo de fechas
    
    * Agrega funcionalidad para mostrar separador de mensajes por día
    
    * Agrega funcionalida para mostrar hora en burbuja de mensaje
    
    * Soluciona conflictos

--- Bubble/View/ChatsViews/PrivateChatView.swift
+++ Bubble/View/ChatsViews/PrivateChatView.swift
@@ -18,57 +18,68 @@ struct PrivateChatView: View {
     var body: some View {
         if let user = chatsViewModel.user {
             VStack {
-                ScrollView {
-                    LazyVStack {
-                        ForEach(privateChatViewModel.messages, id: \.self) {
-                            message in
-                            if message.type == .friendRequest {
-                                Text(
-                                    privateChatViewModel
-                                        .checkIfMessageWasSentByCurrentUser(
-                                            message)
-                                        ? "Le enviaste una solicitud a \(user.nickname)"
-                                        : "\(user.nickname) te envió una solicitud de amistad"
-                                )
-                                .font(.caption)
-                                .foregroundStyle(.secondary)
-                            }
-
-                            if message.type == .acceptedFriendRequest {
-                                Text("Tú y \(user.nickname) ahora son amigos")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-
-                            if message.type == .text {
-                                MessageBubbleView(message: message)
-
+                ScrollViewReader { proxy in
+                    ScrollView {
+                        LazyVStack {
+                            // Cada grupo de mensajes (por día)
+                            ForEach(privateChatViewModel.groupedMessages, id: \.key) { group in
+                                // Separador por día
+                                HStack(spacing: 8) {
+                                    line
+                                    Text(privateChatViewModel.dateHeader(for: group.key))
+                                    line
+                                }
+                                .foregroundStyle(Color.secondary)
+                                .font(.caption2)
+                                .padding(.top, 20)
+                                .padding(.horizontal, 10)
+                                
+                                // Mensajes correspondientes a la fecha
+                                ForEach(group.value, id: \.self) { message in
+                                    if message.type == .friendRequest {
+                                        Text(privateChatViewModel.checkIfMessageWasSentByCurrentUser(message)
+                                             ? "Le enviaste una solicitud a \(user.nickname)"
+                                             : "\(user.nickname) te envió una solicitud de amistad")
+                                        .font(.footnote)
+                                        .foregroundStyle(.secondary)
+                                        .italic()
+                                    }
+                                    
+                                    if message.type == .acceptedFriendRequest {
+                                        Text("Tú y \(user.nickname) ahora son amigos")
+                                            .font(.caption)
+                                            .foregroundStyle(.secondary)
+                                    }
+                                    
+                                    if message.type == .text {
+                                        MessageBubbleView(message: message)
+                                    }
+                                }
                             }
                         }
                         .padding(.bottom, 20)
-                    }
-                    .onChange(of: privateChatViewModel.lastMessage) { _, lastMessage in
-                        withAnimation { }
-                        proxy.scrollTo(lastMessage, anchor: .bottom)
+                        .onChange(of: privateChatViewModel.lastMessage) { _, lastMessage in
+                            withAnimation {
+                                proxy.scrollTo(lastMessage, anchor: .bottom)
+                            }
+                        }
                     }
                 }
-
+                
                 Spacer()
-
+                
                 ZStack(alignment: .bottomTrailing) {
                     TextField("Escribe tu mensaje", text: $messageText)
                         .padding(.trailing, 20)
                         .onSubmit {
                             Task {
                                 if !messageText.isEmpty {
-                                    await privateChatViewModel.sendMessage(
-                                        chatID: chat.id,
-                                        messageText: messageText)
+                                    await privateChatViewModel.sendMessage(chatID: chat.id, messageText: messageText)
                                     messageText = ""
                                 }
                             }
                         }
-
+                    
                     if !messageText.isEmpty {
                         Button {
                             messageText = ""
@@ -91,25 +102,12 @@ struct PrivateChatView: View {
             }
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
-                    Menu {
-                        Button {
-
-                        } label: {
-                            Label(
-                                "Bloquear a \(user.nickname)",
-                                systemImage: "hand.raised")
-                        }
-
-                        Button {
-
-                        } label: {
-                            Label("Personalizar", systemImage: "paintpalette")
-                        }
-
+                    Button {
+                        
                     } label: {
                         Image(systemName: "ellipsis.circle")
                     }
-
+                    
                 }
             }
             .navigationTitle(user.nickname)
