293324ec04ad768141fcd7971e2e5656ea16cc40
lordzzz <lordzzz8@gmail.com>
2025-05-19 23:53:19 +0200

    Se realizan metodos del CRUD en la capa de Service

--- Bubble/Services/PrivateChatService.swift
+++ Bubble/Services/PrivateChatService.swift
@@ -23,7 +23,6 @@ actor PrivateChatService {
     private let uid = Auth.auth().currentUser?.uid ?? ""
     private var listenerRegistration: ListenerRegistration?
     
-    
     /// Obtiene los chats en tiempo real en los que el usuario participa.
     /// - Returns: Un `AsyncThrowingStream` que emite un array de `ChatModel` y maneja errores.
     func getChats() -> AsyncThrowingStream<[ChatModel], Error>  {
@@ -229,4 +228,46 @@ actor PrivateChatService {
         let snapshot = try await docRef.getDocument()
         return try snapshot.data(as: UserModel.self)
     }
+   
+    /// Actualiza el contenido de un mensaje específico por ID.
+    func editMessage(chatsID: String, messageID: String, newContent: String) async throws {
+        let chatRef = Firestore.firestore().collection("chats").document(chatsID)
+        let messageRef = chatRef.collection("messages").document(messageID)
+
+        do{
+            try await messageRef.updateData(["content": newContent])
+            print("Editando mensaje con ID: \(messageID)")
+        }catch{
+            print("Error desde server: No se pudo editar: ")
+            throw error
+        }
+    }
+    
+    /// Marca un mensaje como eliminado, sin borrarlo físicamente.
+    func deleteMessage(chatID: String, messgeID: String) async throws {
+        let chatRef = Firestore.firestore().collection("chats").document(chatID)
+        let messageRef = chatRef.collection("messages").document(messgeID)
+        
+        do{
+            try await messageRef.updateData(["content": "Mensaje eliminado"])
+        }catch{
+            print("Mensaje del server -> Error, el mensaje no se ha actualizado")
+            throw error
+        }
+    }
+    
+    /// Elimina físicamente un mensaje de Firestore.
+    func permanentlyDeleteMessage(chatID: String, messageID: String) async throws {
+        let chatRef = Firestore.firestore().collection("chats").document(chatID)
+        let messageRef = chatRef.collection("messages").document(messageID)
+        
+        do{
+            try await messageRef.delete()
+            print("Mensaje eliminado con exito")
+        }catch{
+            print("Mensaje del server -> Error Al eliminar mensaje: ")
+            throw error
+        }
+    }
+
 }
